bf8f08927eb87a6e28106f2cc7aa8d49c9dca7ad
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2020-05-26 11:27:44 +0300

    Added tests for functions.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,9 +3,22 @@
 #include "../include/functions.h"
 #include "../include/globals.h"
 
-TEST_CASE( "Tetromino rotation function", "[rotate]" ) {
-
+TEST_CASE( "Tetromino pixel rotation function", "[rotate]" ) {
     REQUIRE( rotate(0, 0, 0) == 0 );
     REQUIRE( rotate(0, 0, 1) == 12 );
+    REQUIRE( rotate(3, 0, 1) == 0 );
+}
 
+TEST_CASE( "Tetromino string rotation function", "[rotateTetromino]" ) {
+    REQUIRE( rotateTetromino(tetromino[2], 2) == ".....XX..XX....." );
+    REQUIRE( rotateTetromino(tetromino[1], 3) == "....XXX..X......" );
+    REQUIRE( rotateTetromino(tetromino[5], 1) == ".....XXX.X......" );
+    REQUIRE( rotateTetromino(tetromino[6], 2) == ".....XX..X...X.." );
+}
+
+TEST_CASE( "Custom power function for integers", "[power]" ) {
+    REQUIRE( power(10, 0) == 1 );
+
+    REQUIRE( power(2, 5) == 32 );
+    REQUIRE( power(1, 10) == 1 );
 }
